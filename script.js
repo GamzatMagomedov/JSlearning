ff589eb7bfc211946423b650bcf3f361cfd3a9fa
Gamzat <magomedovgm1@mail.ru>
2018-12-13 22:00:48 +0300

    Это первый комит

---
+++ script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// console.log(leftBorderWidth);
+
+
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+// console.log(leftBorderWidth);
+
+// let person = {
+//     name: "John",
+//     age: 30,
+//     isMarried: false
+// };
+// console.log(person["name"]);
+// console.log(person.name);
+
+// var photos = ["plum", "svg", "png", "jpeg", 45];
+// console.log(photos[0]);
+
+//alert("fddddd");
+
+// let answer = confirm("What are you doing?");
+// console.log(answer);
+
+// let answer = prompt("18+ ?", "da");
+// console.log(typeof(photos));
+
+// console.log("arr"+"obkekt");
+// console.log(4+"obkekt");
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(++incr + " " + --decr);
+console.log(10%4);
+
+console.log("2" === 2);
+console.log("2" == 2);
+
+let isChesked = true,
+    isClose = false;
+ console.log(" ");   
+console.log(!isChesked || isClose);
